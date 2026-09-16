37f90d58b18e741896af839f2d1ac502f5678558
Peruchena <mateoperuchena@gmail.com>
2026-09-16 18:09:41 -0300

    github pages config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import react from '@vitejs/plugin-react'
+// vite.config.js
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/SWUnfolded/',  // ← Reemplaza con el nombre de tu repositorio
 })
